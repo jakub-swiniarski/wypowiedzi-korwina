d74c8c5a00028dba913bff9e293718e3f97c42c6
MisterField <panpole91@gmail.com>
2022-06-03 21:28:06 +0200

    finished

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ fstream plik;
 
 int main()
 {
-    //rand i, cout word[i];
     srand(time(NULL));
     x=rand()%22; //max 21 bo przesuwamy linie w dol o 1, wiec zaczynajac od 1 max bedzie 22
     plik.open("1.txt",ios::in);
@@ -28,7 +27,43 @@ int main()
     x=rand()%22;
     for(int i=0; i<x; i++)
     {
-        getline(plik, linia); //tutaj przesuwamy linie o 1 w dol
+        getline(plik, linia);
+    }
+    cout<<linia;
+    plik.close();
+    plik.open("3.txt",ios::in);
+    srand(time(NULL));
+    x=rand()%22;
+    for(int i=0; i<x; i++)
+    {
+        getline(plik, linia);
+    }
+    cout<<linia;
+    plik.close();
+    plik.open("4.txt",ios::in);
+    srand(time(NULL));
+    x=rand()%22;
+    for(int i=0; i<x; i++)
+    {
+        getline(plik, linia);
+    }
+    cout<<linia;
+    plik.close();
+    plik.open("5.txt",ios::in);
+    srand(time(NULL));
+    x=rand()%22;
+    for(int i=0; i<x; i++)
+    {
+        getline(plik, linia);
+    }
+    cout<<linia;
+    plik.close();
+    plik.open("6.txt",ios::in);
+    srand(time(NULL));
+    x=rand()%22;
+    for(int i=0; i<x; i++)
+    {
+        getline(plik, linia);
     }
     cout<<linia;
     return 0;
